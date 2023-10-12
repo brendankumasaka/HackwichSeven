c4d1e8d583f3ec97bcbab56ba4d63b302ecd5d60
Brendan Kumasaka <bhk22@hawaii.edu>
2023-10-12 13:29:20 -1000

    Initial Commit

---
+++ Hackwich_Seven/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackwich_Seven
+//
+//  Created by Brendan Kumasaka on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
